1d89c31a1816bf2c4ffdcbf8910cdc4908cb1092
Chobilet <afmartinez1215@gmail.com>
2021-05-10 11:16:48 -0700

    Analyzing real data points and simulating against it

--- Simulation.R
+++ Simulation.R
@@ -1,78 +1,107 @@
 source("InternetDist.R")
 
 
-genNums <- function(n,dist){
-  mat <- matrix(data = replicate(n,rand_vect(10,100,9)),nrow = n,ncol = 10, byrow = T)
+genNums <- function(n,sd){
+  mat <- matrix(data = replicate(n,rand_vect(10,100,sd)),nrow = n,ncol = 10, byrow = T)
   return(mat)
 }
 
-playGame<- function(Mat,vec, win,lose,ties){
+playGame<- function(Mat){
   n = nrow(Mat)
+  score <- numeric(n)
+  win <- numeric(n)
+  lose <- numeric(n)
+  ties <- numeric(n)
   for(i in 1:n){
     for(j in i:n){
       if(i != j){
         temp <- Mat[i,] - Mat[j,]
-        playerB <- sum(temp <= 0)
-        playerA <- sum(temp >= 0)
-        if(playerB >  playerA ){
-          vec[j] = vec[j] + 1
-          win[j] = win[j] +1
-          lose[i] = lose[i] + 1
-        }
-        else if(playerA > playerB){
-          vec[i] = vec[i] + 1
+        playerB <- sum(temp < 0)
+        playerA <- sum(temp > 0)
+        if(playerA > playerB){
+          score[i] = score[i] + 1
           win[i] = win[i] + 1
           lose[j] = lose[j] + 1
         }
+        else if(playerB >  playerA ){
+          score[j] = score[j] + 1
+          win[j] = win[j] +1
+          lose[i] = lose[i] + 1
+        }
         else{
-          vec[i] = vec[i] + 0.5
-          vec[j] = vec[j] + 0.5
+          score[i] = score[i] + 0.5
+          score[j] = score[j] + 0.5
           ties[i] = ties[i] + 1
           ties[j] = ties[j] + 1
         }
       }
     }
   }
-  gameDF<- data.frame(vec,win,lose,ties)
+  gameDF<- data.frame(score,win,lose,ties,Mat)
   return(gameDF)
 }
 
 
-winning.Scores <- read.csv("blottoscores_bonus_piazza.csv")
+winning.Scores <- read.csv("blottoscores_bonus_piazza_fixed.csv")
 winning.Scores <- (as.matrix(winning.Scores))
 winning.Scores <- subset(winning.Scores,select= c(8:17))
 winning.Scores <- mapply(winning.Scores, FUN=as.numeric)
 winning.Scores <- matrix(data=winning.Scores,ncol = 10)
-scoreVec <- numeric(nrow(winning.Scores))
-winVec <-numeric(nrow(winning.Scores))
-loseVec <-numeric(nrow(winning.Scores))
-tiesVec <-numeric(nrow(winning.Scores))
-
-classDF <-  playGame(winning.Scores,scoreVec,winVec,loseVec,tiesVec )
 
 
+classDF <-  playGame(winning.Scores)
+#classDF <-  playGame(matrix(mapply(winning.Scores[,8:17],FUN=as.numeric), ncol= 10))
 
 
 R = 10
 rounds = 0
+sd = 1
 repeat{
-  best_vec <- list(numeric(10), 0)
+  best_vec <- list(0,numeric(10))
   
   n = 1000
   mat <- genNums(n)
-  vec <- numeric(n)
-  vec <- playGame(mat,vec)
-  which.max(vec)
-  max(vec)
-  win_vec <- list(mat[which.max(vec),], max(vec))
-  if(best_vec[[2]] < win_vec[[2]]){
-    best_vec <- win_vec
-  }
-  rounds = rounds +1
-  if(rounds == R){
+  df<- as.data.frame(mat)
+  
+  df[(n+1):(n+nrow(winning.Scores)),] <- winning.Scores[1:77,]
+  df <- playGame(mat)
+  which.max(df$score)
+  as.numeric(df[which.max(df$score),5:14])
+  win_vec <- list(as.numeric(which.max(df$score)),as.vector(as.numeric(df[which.max(df$score),5:14])))
+  # if(best_vec[[1]]< win_vec[[1]]){
+  #   best_vec <- win_vec
+  # }
+  winning.Scores[77,] <- win_vec[[2]]
+  classDF <-  playGame(winning.Scores)
+  if(which.max(classDF$score) == 77){
     break
   }
 }
+n = 1
+i = 1
+sd=1
+highest_vec = numeric(10)
+repeat{
+  
+  if(i > 100){
+    sd = sd + 1
+    i = 0
+  }
+  mat <- genNums(n,sd)
+  df<- as.data.frame(mat)
+  
+  df[(n+1):(n+nrow(winning.Scores)),] <- winning.Scores[1:77,]
+  results <- playGame(df)
+  #which.max(df$score)
+  #as.numeric(df[which.max(df$score),5:14])
 
-
-
+  if(which.max(results$score) == 1){
+    highest_vec <- as.numeric(results[which.max(results$score),5:14])
+    break
+  }
+  if(sd > 15){
+    sd = 1
+  }
+  i = i + 1
+}
+df[1,1:10] <- c()
